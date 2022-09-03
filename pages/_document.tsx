17219246ccc07d7b4d4d479a034e90a71d0314a6
Lucas Piputto <lucaspiputto@gmail.com>
2022-09-03 15:56:30 -0300

    Change font families

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>
         <link 
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" 
-          rel="stylesheet" 
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&family=Roboto:wght@300;400;700&display=swap" 
+          rel="stylesheet"
         />
         </Head>
         <body>
