2f3db6ae1d95c8712824fdd93be36734cebeaec7
Lucas Piputto <lucaspiputto@gmail.com>
2022-09-27 13:33:14 -0300

    Update frontend

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,31 @@
-import { NextPage } from 'next';
+import { NextPage } from 'next'
 
 const Login: NextPage = () => {
+  const handleSubmit = (ev: any) => {
+    ev.preventDefault()
+    const formData = Object.fromEntries(new FormData(ev.target))
+    console.log(formData)
+  }
+
   return (
-    <div>
-      login
+    <div className="container center-screen">
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="text" 
+          id="email"
+          name="email"
+          required
+        />
+        <label htmlFor="password">Contraseña: </label>
+        <input 
+          type="password" 
+          id="password"
+          name="password"
+          required
+        />
+        <button type="submit">Iniciar Sesión</button>
+      </form>
     </div>
   )
 }
