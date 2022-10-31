b8b9fed5c9acf3afda693abef9d0f330945bc0b5
Lucas Piputto <lucaspiputto@gmail.com>
2022-10-31 18:11:07 -0300

    Refactor code

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,33 +1,41 @@
 import prisma from './prisma'
 import bcrypt from 'bcrypt'
+
 export async function createUser(name: string, email: string, password: string) {
-  const salt = await bcrypt.genSalt(10)
-  let passwordHash = await bcrypt.hash(password, salt);
+  const saltRounds = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, saltRounds)
   let user = await prisma.user.create({
     data: {
-      name: name,
-      email: email,
-      //@ts-ignore
-      password: passwordHash,
-    },
+      name,
+      email,
+      password: passwordHash
+    }
   })
   //@ts-ignore
   delete user["password"]
   return user
 }
-export async function getUser(email: string, password: string) {
 
+export async function getAllUsers() {
+  try {
+    let users = await prisma.user.findMany()
+    return [users, null]
+  } catch (error) {
+    console.error(error)
+    return [null, error]
+  }
+}
+
+export async function getUser(email: string, password: string) {
   let user = await prisma.user.findUnique({
     where: {
-      email: email
+      email
     }
   })
   if (!user) return null
-  //@ts-ignore
   let match = await bcrypt.compare(password, user.password)
   //@ts-ignore
   delete user["password"]
   if (match) return user
-  return null;
-
+  return null
 }
--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,16 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import bcrypt from 'bcrypt';
-import { createUser, getUser } from '../../../lib/auth';
-type Data = {
-  password: string
-}
+import { getUser } from 'lib/auth'
+import errorHandler from 'middleware/errorHandler'
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse//<Data>
+  res: NextApiResponse
 ) {
-  if (req.method != "POST") return res.status(405).send("Method not allowed.");
-  let { email, password } = req.body;
-  let user = await getUser(email, password)
-  res.status(200).json({ user })
+  if (req.method != "POST") return res.status(405).json({ error: 'Method not allowed' })
+  const { email, password } = req.body
+
+  try {
+    const user = await getUser(email, password)
+    res.status(200).json(user)
+  } catch (error) {
+    errorHandler(error, req, res)
+  }
 }
