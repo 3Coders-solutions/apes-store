7a356c625bbe22669dadff5262574ac65de98934
Lucas <lucaspiputto@gmail.com>
2022-11-22 14:23:06 -0300

    Fix problem with index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import type { NextPage } from 'next'
+import { NextPage } from 'next'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
-import Clothing from "./clothing/index.tsx"
+import Banner from '@components/Banner'
 import HeaderNav from '@components/HeaderNav'
 import FooterNav from '@components/FooterNav'
-
+import ShopItemCard from '@components/ShopItemCard'
 
 
 const Home: NextPage = () => {
+  const [clothes, setClothes] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState('')
+  
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products?limit=5')
+      .then(res => res.json())
+      .then(data => {
+        setClothes(data)
+        setIsLoading(false)
+        console.log(clothes)
+      })
+      .catch(error => {
+        setError(error.message)
+      })
+  })
+
   return (
     <>
       <Head>
@@ -15,7 +33,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <HeaderNav />
-      <Clothing />
+      <Banner 
+        title="Summer Sale"
+        imgSrc="/img/banner-bg1.jpg"
+        imgAlt="Logo Image"
+      />
+      
+      {isLoading ? (
+        <p>cargando productos...</p>
+      ) : (
+        <div style={{ display: "flex"}}>
+          {clothes.map((item: any) => 
+            <ShopItemCard 
+            id={item.id}
+            name={item.title} 
+            description={item.description} 
+            imgSrc={item.image} 
+            price={item.price}
+            key={item.id}
+          />
+          )}
+        </div>
+      )}
+      {error ? ( <p>{error}</p> ) : null}
       <FooterNav />
     </>
   )
